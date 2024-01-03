956fdeb555e329845ba1376ed9b1f5700b3ccc76
yashmehtakristal <144411620+yashmehtakristal@users.noreply.github.com>
2024-01-03 11:40:36 +0800

    Update part2_asset_score_calculator.py

--- core/part2_asset_score_calculator.py
+++ core/part2_asset_score_calculator.py
@@ -8,7 +8,7 @@ import ffn
 import io
 import streamlit as st
 import base64 
-from cryptography.fernet import Fernet
+#from cryptography.fernet import Fernet
 
 @st.cache_data(show_spinner = False)
 def reading_returns_file():
